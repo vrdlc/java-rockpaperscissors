52b3d3e146a798b84b426827d62d7e88dbff0339
isabelle george and Ramon de la Cruz <10izzygeorge@gmail.com>
2016-02-10 09:19:45 -0800

    pass first test for determining rovck beats scissors

---
+++ src/main/java/RPS.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
+
+public class RPS {
+  public static void main(String[] args) {
+
+  }
+  public static Boolean checkWinner(String player, String comp){
+    if (player == "Rock" && comp == "Scissors"){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ src/test/java/RPSTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class RPSTest {
+
+  @Test
+  public void checkWinner_returnsTrueIfRockBeatsScissors_true() {
+    RPS rpsTest = new RPS();
+    assertEquals(true, rpsTest.checkWinner("Rock", "Scissors"));
+  }
+
+  // @Test
+  // public void checkWinner_returnsTrueIfScissorsBeatsPaper_true() {
+  //   RPS rpsTest = new RPS();
+  //   assertEquals(true, rpsTest.checkWinner("Scissors", "Paper"));
+  // }
+}
+
+
+
+
+
+
+
+
+
+//Rock & Rock, Rock & Paper, Rock & Scissors
+//Paper & Paper, Paper & Scissors
+//Scissors & Scissors
